ff8632463d6e4cf60da06afc76e22866bdc3316f
Rafal <rafal.opial@coderslab.pl>
2021-01-30 12:39:20 +0100

    m7d1t3z1 Book entity

---
+++ src/main/java/pl/coderslab/spring01hibernatekrajeew06/entity/Book.java
@@ -0,0 +1,50 @@
+package pl.coderslab.spring01hibernatekrajeew06.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private int rating;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public Book setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Book setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public Book setRating(int rating) {
+        this.rating = rating;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Book setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+}
