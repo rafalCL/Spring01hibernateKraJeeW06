245c08b253dd3841316fc3002d1837200c8e4cb8
Rafal <rafal.opial@coderslab.pl>
2021-01-30 10:30:00 +0100

    m7d1t2z1 czesc 2 - konfiguracja Hibernate

--- src/main/java/pl/coderslab/spring01hibernatekrajeew06/config/AppConfig.java
+++ src/main/java/pl/coderslab/spring01hibernatekrajeew06/config/AppConfig.java
@@ -3,15 +3,21 @@ package pl.coderslab.spring01hibernatekrajeew06.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.persistence.EntityManagerFactory;
+
 @Configuration
 @ComponentScan(basePackages = {"pl.coderslab.spring01hibernatekrajeew06"})
 @EnableWebMvc
+@EnableTransactionManagement
 public class AppConfig implements WebMvcConfigurer {
     @Bean
     public ViewResolver viewResolver() {
@@ -25,5 +31,22 @@ public class AppConfig implements WebMvcConfigurer {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean entityManagerFactoryBean
+                = new LocalEntityManagerFactoryBean();
+        entityManagerFactoryBean.setPersistenceUnitName("spring01hibernatekrajeew06");
+
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager jpaTransactionManager =
+                new JpaTransactionManager(entityManagerFactory);
+
+        return jpaTransactionManager;
     }
 }
---
+++ src/main/resources/META-INF/sql/db.sql
@@ -0,0 +1,7 @@
+DROP DATABASE spring01hibernatekrajeew06;
+
+CREATE DATABASE spring01hibernatekrajeew06
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE spring01hibernatekrajeew06;
