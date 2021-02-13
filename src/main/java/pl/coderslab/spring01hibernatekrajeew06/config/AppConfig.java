84c13794a436aaecc65c745a4a18f438f6e014bc
Rafal <rafal.opial@coderslab.pl>
2021-02-13 09:50:42 +0100

    m7d3t1z1 configure hibernate-validator

--- src/main/java/pl/coderslab/spring01hibernatekrajeew06/config/AppConfig.java
+++ src/main/java/pl/coderslab/spring01hibernatekrajeew06/config/AppConfig.java
@@ -7,14 +7,17 @@ import org.springframework.format.FormatterRegistry;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import pl.coderslab.spring01hibernatekrajeew06.converter.AuthorConverter;
 
 import javax.persistence.EntityManagerFactory;
+import java.util.Locale;
 
 @Configuration
 @ComponentScan(basePackages = {"pl.coderslab.spring01hibernatekrajeew06"})
@@ -60,5 +63,12 @@ public class AppConfig implements WebMvcConfigurer {
     @Bean
     public AuthorConverter authorConverter(){
         return new AuthorConverter();
+    }
+
+    @Bean(name="localeResolver")
+    public LocaleContextResolver getLocaleContextResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pl","PL"));
+        return localeResolver;
     }
 }
