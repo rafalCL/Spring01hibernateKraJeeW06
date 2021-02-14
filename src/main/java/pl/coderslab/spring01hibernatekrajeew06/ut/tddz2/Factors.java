94e3d440114940c488c43ae0bdf9cd9d40fd0b99
Rafal <rafal.opial@coderslab.pl>
2021-02-14 16:30:33 +0100

    m7d4t4z2 part1 Add method declaration

---
+++ src/main/java/pl/coderslab/spring01hibernatekrajeew06/ut/tddz2/Factors.java
@@ -0,0 +1,12 @@
+package pl.coderslab.spring01hibernatekrajeew06.ut.tddz2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Factors {
+    public static List<Integer> generatePrimeFactors(int n){
+        List<Integer> result = new ArrayList<>();
+
+        return result;
+    }
+}
