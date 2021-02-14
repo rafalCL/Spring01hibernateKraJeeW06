38a57d80a81fc5e5355fd5bf83e1838617abb242
Rafal <rafal.opial@coderslab.pl>
2021-02-14 16:43:03 +0100

    m7d4t4z2 part3 Implement method to pass test

--- src/main/java/pl/coderslab/spring01hibernatekrajeew06/ut/tddz2/Factors.java
+++ src/main/java/pl/coderslab/spring01hibernatekrajeew06/ut/tddz2/Factors.java
@@ -6,6 +6,15 @@ import java.util.List;
 public class Factors {
     public static List<Integer> generatePrimeFactors(int n){
         List<Integer> result = new ArrayList<>();
+        result.add(1);
+
+        for(int i = 2; i<=n/2; i++){
+            if (n % i == 0){
+                result.add(i);
+            }
+        }
+
+        result.add(n);
 
         return result;
     }
